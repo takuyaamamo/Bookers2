13f030a9a26f27fcf883481566335d618be4caf1
Ama <Ama@amacbook.local>
2019-04-15 16:25:26 +0900

    [update]spplication_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  # deviseコントローラーの修正はここに書く
+  # nameカラムを追加したため修正が必要となった
+  # deviseを実行する前に:configure_permitted_parametersを呼び出し
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # deviseでストロングパラメータを追加する追加しないとPOSTに反映されない？
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+  end
 end
