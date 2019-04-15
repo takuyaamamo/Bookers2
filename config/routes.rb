239f1586573b5c54cd4192084dd366274dc8b94a
Ama <Ama@amacbook.local>
2019-04-15 15:37:14 +0900

    [update]add setting for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  # deviseを使用する際にはURLにusersを含む
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
