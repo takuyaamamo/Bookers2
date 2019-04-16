2a635119dbd742ad6e111ca10c9680f5975ef23b
Ama <Ama@amacbook.local>
2019-04-16 17:02:44 +0900

    [update]rails g devise:controllers users

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,14 @@ Rails.application.routes.draw do
 
   resources :user_images, only: [:new, :create]
   resources :users, only: [:show]
+
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy"
+  end
 end
