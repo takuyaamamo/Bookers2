def47bc7a925b98f84979252b1b2e649bbdba440
Ama <Ama@amacbook.local>
2019-04-16 14:20:31 +0900

    [update]created user_images controller

---
+++ app/controllers/user_images_controller.rb
@@ -0,0 +1,9 @@
+class UserImagesController < ApplicationController
+
+  def new ##画像投稿画面表示
+  end
+
+  def create ##投稿画像保存
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # トップページをルートにするroot to: 'コントローラ#アクション'
   root to: 'toppage#top'
-  
+
   get 'toppage/top'
   # deviseを使用する際にはURLにusersを含む
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :user_images, only: [:new, :create]
 end
