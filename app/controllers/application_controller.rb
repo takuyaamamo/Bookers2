02777e91b3ec4cd025139110882c3d1a78e6a0a1
Ama <Ama@amacbook.local>
2019-04-16 16:30:07 +0900

    [fix]Corrected the screen transition after sign-in and sign-out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,20 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # 新規登録時とログイン時にdeviseでストロングパラメータを追加する.追加しないとPOSTに反映されない
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+  end
+
+# deviseはデフォルト設定でログイン後トップページに飛んでしまう。トップページを設定している場合変更は出来ない。よって以下の設定を行う
+  def after_sign_in_path_for(resource)
+    user_path(current_user.id)
+  end
+
+  def after_sign_up_path_for(resource)
+    user_path(current_user.id)
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
   # トップページをルートにするroot to: 'コントローラ#アクション'
   root to: 'toppage#top'
+
+  # deviceでサインイン後のページをトップにしていする
+  # root 'users#show'
 
   get 'toppage/top'
   # deviseを使用する際にはURLにusersを含む
@@ -9,4 +11,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :user_images, only: [:new, :create]
+  resources :users, only: [:show]
 end
