f28c687a9a09becd955f094f067ce56f49bfe014
Ama <Ama@amacbook.local>
2019-04-16 19:23:30 +0900

    [update]destroy UserImages controller & UserImages model

--- app/controllers/user_images_controller.rb
+++
@@ -1,9 +0,0 @@
-class UserImagesController < ApplicationController
-
-  def new ##画像投稿画面表示
-  end
-
-  def create ##投稿画像保存
-  end
-
-end
--- db/schema.rb
+++ db/schema.rb
@@ -20,14 +20,6 @@ ActiveRecord::Schema.define(version: 2019_04_16_052439) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "user_images", force: :cascade do |t|
-    t.text "image_id"
-    t.integer "user_id"
-    t.text "caption"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
